096965a1bb634e01f4fc4f07290fbed0664fd66f
Loucas-Tougeron <louc85@hotmail.fr>
2025-11-25 15:19:19 +0100

    fix: ajout de l'import dans le module user

--- back/src/modules/users/users.module.ts
+++ back/src/modules/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { User } from '../../infrastructure/entities/user.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]) // Importer le repository User
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService]
 })
 export class UsersModule {}
