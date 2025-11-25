bb3aa2752ed2920df507238affeb4daf42fc2e63
JulienPajot <julien.pajot@dupdevinci-edu.fr>
2025-11-25 15:22:18 +0100

    feat: add annoncements CRUD and getAll and getById

---
+++ back/src/modules/announcements/announcements.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AnnouncementsService } from './announcements.service';
+import { AnnouncementsController } from './announcements.controller';
+import { Announcement } from 'src/infrastructure/entities/announcement.entity';
+import { User } from 'src/infrastructure/entities/user.entity';
+import { Car } from 'src/infrastructure/entities/car.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Announcement, User, Car])],
+  controllers: [AnnouncementsController],
+  providers: [AnnouncementsService],
+})
+export class AnnouncementsModule {}
