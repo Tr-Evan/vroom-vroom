4e73f67023872378c7d80d23d6f297952a0620c6
JulienPajot <julien.pajot@dupdevinci-edu.fr>
2025-11-26 12:16:13 +0100

    update: update annoncements controller

--- back/src/modules/announcements/announcements.controller.ts
+++ back/src/modules/announcements/announcements.controller.ts
@@ -2,28 +2,29 @@ import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common'
 import { AnnouncementsService } from './announcements.service';
 import { Announcement } from 'src/infrastructure/entities/announcement.entity';
 import { CreateAnnouncementDto, UpdateAnnouncementDto } from './announcements.types';
+import { AnnouncementModel } from './announcements.model';
 
 @Controller('announcements')
 export class AnnouncementsController {
   constructor(private readonly announcementsService: AnnouncementsService) {}
 
   @Get()
-  getAll(): Promise<Announcement[]> {
+  getAll(): Promise<AnnouncementModel[]> {
     return this.announcementsService.getAllAnnouncements();
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string): Promise<Announcement | null> {
+  getOne(@Param('id') id: string): Promise<AnnouncementModel | null> {
     return this.announcementsService.getAnnouncementById(+id);
   }
 
   @Post()
-  create(@Body() data: CreateAnnouncementDto): Promise<Announcement> {
+  create(@Body() data: CreateAnnouncementDto): Promise<AnnouncementModel> {
     return this.announcementsService.createAnnouncement(data);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() data: UpdateAnnouncementDto): Promise<Announcement | null> {
+  update(@Param('id') id: string, @Body() data: UpdateAnnouncementDto): Promise<AnnouncementModel | null> {
     return this.announcementsService.updateAnnouncement(+id, data);
   }
 
